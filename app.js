145c7a9eac4a0bbaa931b64762cccc8940a3bfbd
Felipe Freitas <canais.ffasilveira@voegol.com.br>
2025-03-26 09:00:24 -0300

    feat: allow CORS for custom domain

--- app.js
+++ app.js
@@ -15,9 +15,13 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 app.use(compression());
 
-app.use(cors({
-  origin: '*', 
-}))
+app.use(
+  cors({
+    origin: ["https://saintseiyaapi.com"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
