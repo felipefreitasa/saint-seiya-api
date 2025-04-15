115bbc92bbd807370eb44dce24650463e55a8e5a
Felipe Freitas <canais.ffasilveira@voegol.com.br>
2025-04-15 10:15:43 -0300

    refactor: refactoring swagger

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -1,22 +1,6 @@
-const swaggerJsDoc = require("swagger-jsdoc");
+const YAML = require('yamljs');
+const path = require('path');
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Saint Seiya API",
-      version: "1.0.0",
-      description: "A complete API to access detailed information about the characters from the anime Saint Seiya.",
-    },
-    servers: [
-      {
-        url: "https://www.saintseiyaapi.com/api",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
-
-const swaggerSpecs = swaggerJsDoc(options);
+const swaggerSpecs = YAML.load(path.join(__dirname, './docs/swagger.yaml'));
 
 module.exports = swaggerSpecs;
